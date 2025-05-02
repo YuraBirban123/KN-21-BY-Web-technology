0429364f6b1837a4bffa7da28444fef73bd3c55b
YuraBirban123 <urabirban123@gmail.com>
2025-05-02 11:20:33 +0300

    modified lab7

--- Lab7/script.js
+++ Lab7/script.js
@@ -1,11 +1,15 @@
+let colorInterval;
+
 function animateCircle() {
   const circle = document.getElementById("circle");
   circle.classList.remove("animate");
-  void circle.offsetWidth; // Перезапуск анімації
+  void circle.offsetWidth;
   circle.classList.add("animate");
 
-  const N = 1; // Номер варіанту
-  setInterval(() => {
+  const N = 1;
+
+  clearInterval(colorInterval); // очищення попереднього
+  colorInterval = setInterval(() => {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     circle.style.backgroundColor = randomColor;
   }, N * 1000);
